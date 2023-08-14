0bdf05daf01a1e2f348b9ace1658595fff7f4192
GODrums <armin.stanitzok@outlook.de>
2023-08-14 18:12:15 +0200

    Request host permissions if necessary

--- src/background.ts
+++ src/background.ts
@@ -6,21 +6,6 @@ chrome.storage.local.get('lastUpdate', (data) => {
     }
 });
 
-// update every 8 hours
-if (lastUpdate < Date.now() - 1000 * 60 * 60 * 8) {
-    fetch('https://prices.csgotrader.app/latest/prices_v6.json')
-        .then((response) => response.json())
-        .then((data) => {
-            chrome.storage.local.set({ prices: JSON.stringify(data) }).then(() => {
-                console.log('Prices updated. Last update: ' + lastUpdate + '. Current time: ' + Date.now());
-            });
-        })
-        .catch((err) => console.error(err));
-
-    lastUpdate = Date.now();
-    chrome.storage.local.set({ lastUpdate: lastUpdate });
-}
-
 // Check whether new version is installed
 chrome.runtime.onInstalled.addListener(function (details) {
     if (details.reason == 'install') {
@@ -35,4 +20,37 @@ chrome.runtime.onInstalled.addListener(function (details) {
         var thisVersion = chrome.runtime.getManifest().version;
         console.log('[BetterFloat] Updated from version ' + details.previousVersion + ' to ' + thisVersion + '!');
     }
+
+    // check if extension has host permissions and request them if not
+    const host_permissions = ['*://prices.csgotrader.app/*']
+    chrome.permissions.contains({
+        origins: host_permissions
+    }).then((result) => {
+        if (!result) {
+            chrome.permissions.request({
+                origins: host_permissions
+            }, (granted) => {
+                if (granted) {
+                    console.log('[BetterFloat] Host Permission granted.');
+                } else {
+                    console.log('[BetterFloat] Host Permission denied. Please enable manually in the extension settings.');
+                }
+            });
+        }
+    });
 });
+
+// update every 8 hours
+if (lastUpdate < Date.now() - 1000 * 60 * 60 * 8) {
+    fetch('https://prices.csgotrader.app/latest/prices_v6.json')
+        .then((response) => response.json())
+        .then((data) => {
+            chrome.storage.local.set({ prices: JSON.stringify(data) }).then(() => {
+                console.log('Prices updated. Last update: ' + lastUpdate + '. Current time: ' + Date.now());
+            });
+        })
+        .catch((err) => console.error(err));
+
+    lastUpdate = Date.now();
+    chrome.storage.local.set({ lastUpdate: lastUpdate });
+}
