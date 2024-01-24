be9f4d017a87587fb2ea43218008204b6c72242a
GODrums <armin.stanitzok@outlook.de>
2024-01-25 00:57:53 +0100

    Release 1.15.0

--- src/skinport/content_script.ts
+++ src/skinport/content_script.ts
@@ -875,7 +875,7 @@ function generateBuffContainer(container: HTMLElement, priceListing: number, pri
     buffPriceAsk.textContent = `Ask ${currencySymbol}${priceListing.toFixed(2)}`;
     buffPrice.appendChild(buffPriceAsk);
     buffContainer.appendChild(buffPrice);
-    if (priceOrder > priceListing * 1.1) {
+    if (priceOrder > priceListing) {
         const warningImage = document.createElement('img');
         warningImage.setAttribute('src', extensionSettings.runtimePublicURL + '/triangle-exclamation-solid.svg');
         warningImage.setAttribute('style', `height: 20px; margin-left: 5px; filter: brightness(0) saturate(100%) invert(28%) sepia(95%) saturate(4997%) hue-rotate(3deg) brightness(103%) contrast(104%);${isItemPage ? 'margin-bottom: 1px;' : ''}`);
