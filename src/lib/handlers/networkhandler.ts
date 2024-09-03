c9d45959beec95644a730e25fa5bfd260cfc4efe
GODrums <armin.stanitzok@outlook.de>
2024-09-03 06:06:05 +0200

    Change CSBlueGem's base URL

--- src/lib/handlers/networkhandler.ts
+++ src/lib/handlers/networkhandler.ts
@@ -4,8 +4,10 @@ import { cacheRealCurrencyRates } from './mappinghandler';
 import { sendToBackground } from '@plasmohq/messaging';
 import type { BlueGem, Extension } from '../@typings/ExtensionTypes';
 
+const CSBLUEGEM_API_URL = 'https://api.csbluegem.com/';
+
 export async function fetchCSBlueGemPatternData(type: string, pattern: number) {
-	return fetch(`https://csbluegem.com/api/v1/patterndata?skin=${type.replace(' ', '_')}&pattern=${pattern}`).then((res) => res.json() as Promise<BlueGem.PatternData>);
+	return fetch(`${CSBLUEGEM_API_URL}/v1/patterndata?skin=${type.replace(' ', '_')}&pattern=${pattern}`).then((res) => res.json() as Promise<BlueGem.PatternData>);
 }
 
 type CSBlueGemOptions = {
@@ -15,7 +17,7 @@ type CSBlueGemOptions = {
 };
 
 export async function fetchCSBlueGemPastSales({ type, paint_seed, currency = 'USD' }: CSBlueGemOptions) {
-	return fetch(`https://csbluegem.com/api/v1/search?skin=${type.replace(' ', '_')}&pattern=${paint_seed}&currency=${currency}`).then((res) => res.json() as Promise<BlueGem.PastSale[]>);
+	return fetch(`${CSBLUEGEM_API_URL}/v1/search?skin=${type.replace(' ', '_')}&pattern=${paint_seed}&currency=${currency}`).then((res) => res.json() as Promise<BlueGem.PastSale[]>);
 }
 
 // fetches currency rates from freecurrencyapi through my api to avoid rate limits
