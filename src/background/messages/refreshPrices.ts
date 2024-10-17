593e282a01e1547f76282097494b31bd336969cc
GODrums <armin.stanitzok@outlook.de>
2024-10-18 00:16:54 +0200

    fix: multi-tab request spam

--- src/background/messages/refreshPrices.ts
+++ src/background/messages/refreshPrices.ts
@@ -9,8 +9,24 @@ type PriceResponse = {
 	status: number;
 };
 
+const lastRequest: Record<string, number> = {};
+
 const handler: PlasmoMessaging.MessageHandler<PriceBody, PriceResponse> = async (req, res) => {
 	const source = req.body?.source;
+	if (!source) {
+		res.send({
+			status: 500,
+		});
+		return;
+	}
+	if (lastRequest[source] > 0 && Date.now() - lastRequest[source] < 1000 * 60 * 10) {
+		console.log('[BetterFloat] Prices were requested within the last 10 minutes. Skipping refresh.');
+		res.send({
+			status: 200,
+		});
+		return;
+	}
+	lastRequest[source] = Date.now();
 	console.log('[BetterFloat] Refreshing prices from source:', source);
 	const pricesURL = `prices${source !== 'buff' ? `_${source}` : ''}`;
 
